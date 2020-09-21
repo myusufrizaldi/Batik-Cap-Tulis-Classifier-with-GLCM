c5c3482590c0c2355e29a2205c626493365bd784
Yusuf <mohamadyusufrizaldi@gmail.com>
2020-09-21 15:40:09 +0700

    (KNN) rancreidml2 new data structure more speed

--- rancreidml2.py
+++ rancreidml2.py
@@ -1,7 +1,9 @@
-#### Dependencies
+####  Local Dependencies
 
 import dec2bin as dtb
 import imageglcm as iglcm
+
+####  Global Dependencies
 
 import glob
 import json
@@ -13,7 +15,9 @@ import random
 import statistics
 import timeit
 
-#### Class
+
+
+####  Classification Methods Class
 
 class GlcmKnnClassifier:
     def __init__(self, model_name, k_neighbors=3, k_folds=5, glcm_components=['contrast', 'correlation', 'energy', 'homogeneity', 'ASM', 'dissimilarity']):
@@ -28,7 +32,7 @@ class GlcmKnnClassifier:
         self.glcm_component_ids         = [self.all_glcm_components.index(glcm_component) for glcm_component in glcm_components]
 
         self.initialize()
-        
+
     def initialize(self):
         self.validation_loss      = 0
         self.validation_accuracy  = 0
@@ -46,8 +50,8 @@ class GlcmKnnClassifier:
         self.class_names     = []
         self.training_data   = []
         self.validation_data = []
-        self.training_sample = []
-        self.testing_sample  = []
+        self.training_sample = {}
+        self.testing_sample  = {}
 
         self.class_training_data_start_ids = []
         self.splitted_training_data = []
@@ -162,39 +166,30 @@ class GlcmKnnClassifier:
         minimum_euclidean_distances = [99999999 for i in range(self.k_neighbors)]
         minimum_img_class_names = ['unknown' for i in range(self.k_neighbors)]
         
-        is_first_loop = True
-        for row in self.training_sample:
-            training_sample_img_class_name = row[0]
-            training_sample_img_features   = row[1:]
-            
-            euclidean_distance = self.get_euclidean_distance(training_sample_img_features, img_features)
-            
-            # Thanks to Allah, then my friend github.com/alien087 on this idea, slightly faster
-            maximum_euclidean_distance    = max(minimum_euclidean_distances)
-            maximum_euclidean_distance_id = minimum_euclidean_distances.index(maximum_euclidean_distance)
-            if(euclidean_distance < maximum_euclidean_distance):
-                minimum_euclidean_distances[maximum_euclidean_distance_id] = euclidean_distance
-                minimum_img_class_names[maximum_euclidean_distance_id] = training_sample_img_class_name
+        for training_sample_img_features in self.training_sample:
+            training_sample_img_class_name = self.training_sample[training_sample_img_features][0]
+            training_sample_img_count      = self.training_sample[training_sample_img_features][1]
 
-            # if(is_first_loop):
-            #     minimum_euclidean_distances[0] = euclidean_distance
-            #     minimum_img_class_names[0]     = training_sample_img_class_name
-            #     is_first_loop = False
-            # else:
-            #     for i in range(self.k_neighbors):
-            #         if(euclidean_distance < minimum_euclidean_distances[i] or minimum_euclidean_distances[i] == -1):
-            #             for j in range(self.k_neighbors - 1, i, -1):
-            #                 minimum_euclidean_distances[j] = minimum_euclidean_distances[j - 1]
-            #                 minimum_img_class_names[j] = minimum_img_class_names[j - 1]
-            #             minimum_euclidean_distances[i] = euclidean_distance
-            #             minimum_img_class_names[i] = training_sample_img_class_name
-            #             break
-                        
+            euclidean_distance = self.get_euclidean_distance(training_sample_img_features, img_features)
+
+            for i in range(training_sample_img_count):
+                if(i == self.k_neighbors):
+                    break
+
+                # Thanks to Allah, then my friend github.com/alien087 (Dino Febriyanto) on this idea, slightly faster
+                maximum_euclidean_distance    = max(minimum_euclidean_distances)
+                maximum_euclidean_distance_id = minimum_euclidean_distances.index(maximum_euclidean_distance)
+                if(euclidean_distance < maximum_euclidean_distance):
+                    minimum_euclidean_distances[maximum_euclidean_distance_id] = euclidean_distance
+                    minimum_img_class_names[maximum_euclidean_distance_id] = training_sample_img_class_name
+                else:
+                    break
+
         minimum_img_class_names = list(filter(lambda val: val != 'unknown', minimum_img_class_names))
         img_class_name = max(set(minimum_img_class_names), key=minimum_img_class_names.count)
         
         return img_class_name
-    
+
     def deep_train(self, k_neighbors_list=[3], training_rate_list=[0.8], iterations_list=[5], epochs_list=[10], is_accuracy_oriented=False, is_full_epochs=True, is_skip_epoch_validation=False):
         print('Deep training has been started. It will take a lot of time, so take your time:)\n')
         print('')
@@ -242,8 +237,8 @@ class GlcmKnnClassifier:
         print('\n')       
         
         best_validation_accuracy = 0
-        best_validation_accuracy_training_sample = []
-        best_validation_accuracy_testing_sample = []
+        best_validation_accuracy_training_sample = {}
+        best_validation_accuracy_testing_sample = {}
         best_validation_accuracy_glcm_components = []
         
         subsets_length_constraint = 2 ** self.all_glcm_components_length
@@ -288,8 +283,8 @@ class GlcmKnnClassifier:
         
     def train(self, training_rate=0.8, epochs=20, is_accuracy_oriented=False, is_full_epochs=False, is_skip_epoch_validation=False):
         print('Training...')
-        self.training_sample       = []
-        self.testing_sample        = []
+        self.training_sample       = {}
+        self.testing_sample        = {}
         training_sample_history    = []
         testing_sample_history     = []
         validation_metrics_history = []
@@ -316,11 +311,25 @@ class GlcmKnnClassifier:
                 available_folds.remove(random_fold_id)
 
                 for class_id in range(splitted_training_data_length):
-                    self.testing_sample += self.splitted_training_data[class_id][random_fold_id]
+                    for training_data_row in self.splitted_training_data[class_id][random_fold_id]:
+                        training_data_class_name   = training_data_row[0]
+                        training_data_row_features = tuple(training_data_row[1:])
+
+                        if(training_data_row_features in self.testing_sample):
+                            self.testing_sample[training_data_row_features][1] += 1
+                        else:
+                            self.testing_sample[training_data_row_features] = [training_data_class_name, 1]
 
             for fold_id in available_folds:
                 for class_id in range(splitted_training_data_length):
-                    self.training_sample += self.splitted_training_data[class_id][fold_id]
+                    for training_data_row in self.splitted_training_data[class_id][fold_id]:
+                        training_data_class_name   = training_data_row[0]
+                        training_data_row_features = tuple(training_data_row[1:])
+
+                        if(training_data_row_features in self.training_sample):
+                            self.training_sample[training_data_row_features][1] += 1
+                        else:
+                            self.training_sample[training_data_row_features] = [training_data_class_name, 1]
 
             training_sample_history.append(self.training_sample)
             testing_sample_history.append(self.testing_sample)
@@ -387,16 +396,17 @@ class GlcmKnnClassifier:
         total_correct_answer = 0
         total_guess = 0
         
-        for row in self.testing_sample:
-            expected_testing_img_class_name = row[0]
-            testing_img_features = row[1:]
-            
-            testing_img_class_name = self.get_img_features_class(testing_img_features)
-            
-            if(expected_testing_img_class_name == testing_img_class_name):
-                total_correct_answer += 1
+        for testing_img_features in self.testing_sample:
+            testing_img_properties = self.testing_sample[testing_img_features]
+            expected_testing_img_class_name = testing_img_properties[0]
+            testing_img_count = testing_img_properties[1]
+
+            testing_img_class_name_prediction = self.get_img_features_class(testing_img_features)
+
+            if(expected_testing_img_class_name == testing_img_class_name_prediction):
+                total_correct_answer += testing_img_count
                 
-            total_guess += 1
+            total_guess += testing_img_count
             
         testing_accuracy = round(total_correct_answer / total_guess, 4)
         testing_loss = 1 - testing_accuracy
@@ -500,24 +510,48 @@ class GlcmKnnClassifier:
         self.glcm_components    = glcm_components
         self.glcm_component_ids = [self.all_glcm_components.index(glcm_component) for glcm_component in glcm_components]
 
+    def get_data_as_str(self, data):
+        data_str = ''
+        data_last_id = len(data) - 1
+        for row_id in range(data_last_id):
+            data_str += str(data[row_id]).replace('\'', '') + '\n'
+        data_str += str(data[data_last_id]).replace('\'', '')
+
+        return data_str
+
+    def get_data_as_list(self, data_str):
+        data_list = []
+        data_str_list = data_str.split('\n')
+        for row in data_str_list:
+            row = row[1:len(row) - 1].split(', ')
+            row[1:] = [float(value) for value in row[1:]]
+            data_list.append(tuple(row))
+
+        return data_list
+
     def get_sample_as_str(self, sample):
         sample_str = ''
-        sample_last_id = len(sample) - 1
-        for row_id in range(sample_last_id):
-            sample_str += str(sample[row_id]).replace('\'', '') + '\n'
-        sample_str += str(sample[sample_last_id]).replace('\'', '')
+
+        for sample_img_features in sample:
+            sample_img_properties = sample[sample_img_features]
+            sample_str += str((sample_img_properties[0], sample_img_properties[1], str(sample_img_features).replace('(', '').replace(')', ''))).replace('\'', '') + '\n'
+        sample_str = sample_str[:len(sample_str) - 2]
 
         return sample_str
 
-    def get_sample_as_list(self, sample_str):
-        sample_list = []
+    def get_sample_as_dict(self, sample_str):
+        sample_dict = {}
         sample_str_list = sample_str.split('\n')
         for row in sample_str_list:
             row = row[1:len(row) - 1].split(', ')
-            row[1:] = [float(value) for value in row[1:]]
-            sample_list.append(tuple(row))
 
-        return sample_list
+            img_class_name = row[0]
+            img_count = row[1]
+            img_features = tuple([float(value) for value in row[2:]])
+
+            sample_dict[img_features] = tuple(img_class_name, img_count)
+
+        return sample_dict
 
     def save(self):
         model_json = {}
@@ -531,8 +565,8 @@ class GlcmKnnClassifier:
         model_json['validation_loss'] = self.validation_loss
         model_json['validation_accuracy'] = self.validation_accuracy
         model_json['class_names'] = self.class_names
-        model_json['training_data'] = self.get_sample_as_str(self.training_data)
-        model_json['validation_data'] = self.get_sample_as_str(self.validation_data)
+        model_json['training_data'] = self.get_data_as_str(self.training_data)
+        model_json['validation_data'] = self.get_data_as_str(self.validation_data)
         model_json['training_sample'] = self.get_sample_as_str(self.training_sample)
         model_json['testing_sample'] = self.get_sample_as_str(self.testing_sample)
         model_json['class_training_data_start_ids'] = self.class_training_data_start_ids
@@ -564,10 +598,10 @@ class GlcmKnnClassifier:
             self.validation_loss = model_data['validation_loss']
             self.validation_accuracy = model_data['validation_accuracy']
             self.class_names = model_data['class_names']
-            self.training_data = self.get_sample_as_list(model_data['training_data'])
-            self.validation_data = self.get_sample_as_list(model_data['validation_data'])
-            self.training_sample = self.get_sample_as_list(model_data['training_sample'])
-            self.testing_sample = self.get_sample_as_list(model_data['testing_sample'])
+            self.training_data = self.get_data_as_list(model_data['training_data'])
+            self.validation_data = self.get_data_as_list(model_data['validation_data'])
+            self.training_sample = self.get_sample_as_dict(model_data['training_sample'])
+            self.testing_sample = self.get_sample_as_dict(model_data['testing_sample'])
             self.class_training_data_start_ids = model_data['class_training_data_start_ids']
             self.split_training_data()
 
@@ -588,4 +622,3 @@ class GlcmKnnClassifier:
             formatted_time += str(seconds) + ' secs'
 
         return formatted_time
-        
